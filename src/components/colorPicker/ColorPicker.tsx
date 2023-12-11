811bcc92ef0dca29cbec69b81f0d1a55dd6a0e93
Ananta Bipal <anantabipal2010@gmail.com>
2023-12-11 18:46:02 +0545

    Out 18 Fix table cell background (#21)
    * placeholder appears in all empty line
    
    * set default editor color to #fff
    
    * added background color text
    
    * fixed table cell background color

--- src/components/colorPicker/ColorPicker.tsx
+++ src/components/colorPicker/ColorPicker.tsx
@@ -26,25 +26,28 @@ const ColorPicker = () => {
 
   return (
     <div
-      className='py-600 px-500 border-1 border-b relative flex justify-end p-4 gap-3 z-0'
+      className='py-600 px-500 border-1 border-b relative flex justify-between p-4 gap-3 z-0 items-center'
       ref={pickerRef}
     >
-      <div
-        className='border border-slate-200 rounded-100 p-1 flex flex-col justify-center cursor-pointer'
-        onClick={() => setShowPicker((prev) => !prev)}
-      >
+      <p className='font-medium'>Background color</p>
+      <div className='flex gap-3'>
         <div
-          style={{
-            backgroundColor: `${appState?.appState.editorColor}`,
-          }}
-          className='w-5 h-5 rounded-050'
-        ></div>
-      </div>
-      <div
-        className='py-1.5 px-3.5 border border-slate-200 text-xs'
-        onClick={() => setShowPicker((prev) => !prev)}
-      >
-        {appState?.appState.editorColor}
+          className='border border-slate-200 rounded p-1 flex flex-col justify-center cursor-pointer'
+          onClick={() => setShowPicker((prev) => !prev)}
+        >
+          <div
+            style={{
+              backgroundColor: `${appState?.appState.editorColor}`,
+            }}
+            className='w-5 h-5 rounded'
+          ></div>
+        </div>
+        <div
+          className='py-1.5 px-3.5 border border-slate-200 text-xs'
+          onClick={() => setShowPicker((prev) => !prev)}
+        >
+          {appState?.appState.editorColor}
+        </div>
       </div>
 
       <When condition={showPicker}>
