83ce8c573058671ca15123952b57aa8c163644b6
aatbip <anantabipal2010@gmail.com>
2023-12-27 10:59:43 +0545

    fix lint error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       spacing: {
-        'none': '0px',
+        none: '0px',
         '025': '1px',
         '050': '2px',
         '100': '4px',
@@ -25,9 +25,8 @@ const config: Config = {
         '1600': '64px',
         '2000': '80px',
         '2400': '96px',
-        '3200': '128px'
+        '3200': '128px',
       },
-
 
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -40,17 +39,17 @@ const config: Config = {
       width: {
         '100': '563px',
         '2xs': '320px',
-        'xs': '384px',
-        'sm': '480px',
-        'md': '560px',
-        'lg': '640px',
-        'xl': '768px',
+        xs: '384px',
+        sm: '480px',
+        md: '560px',
+        lg: '640px',
+        xl: '768px',
         '2xl': '1024px',
         '3xl': '1280px',
         '4xl': '1440px',
         '5px': '1600px',
         '6xl': '1920px',
-        'paragraph-max-width': '720px'
+        'paragraph-max-width': '720px',
       },
       backgroundColor: {
         'new-dark': '#0E0E10',
@@ -58,27 +57,27 @@ const config: Config = {
         'new-card-border': '#EDEDF0',
         'new-white-2': '#F8F9FB',
         'new-white-3': '#EFF1F4',
-        'gray-300': '#C9CBCD'
+        'gray-300': '#C9CBCD',
       },
       textColor: {
         'new-gray': '#60606A',
-        'text': '#212B36',
+        text: '#212B36',
         'gray-300': '#C9CBCD',
         'gray-200': '#DFE1E4',
-        'gray-100': '#C9CBCD'
+        'gray-100': '#C9CBCD',
       },
       borderColor: {
         'col-1': '#DFE1E4',
         'gray-300': '#C9CBCD',
         'gray-200': '#DFE1E4',
-        'gray-100': '#C9CBCD'
+        'gray-100': '#C9CBCD',
       },
       boxShadow: {
         'variant-1': '0px 6px 20px 0px rgba(0, 0, 0, 0.12)',
         'variant-2': ' 0px 8px 24px 0px rgba(0, 0, 0, 0.12)',
       },
       borderRadius: {
-        'none': '0px',
+        none: '0px',
         '050': '2px',
         '100': '4px',
         '150': '6px',
@@ -87,7 +86,7 @@ const config: Config = {
         '400': '16px',
         '500': '20px',
         'rounded-600': '24px',
-        'full': '9999px',
+        full: '9999px',
         'rounded-xl': '35px',
       },
       borderWidth: {
