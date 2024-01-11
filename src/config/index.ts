60ad86851b9f9d8169976cfd88b87676fbf09941
aatbip <anantabipal2010@gmail.com>
2024-01-11 13:31:14 +0545

    updated env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,5 @@
 export const copilotAPIUrl = process.env.COPILOT_API_URL || ''
 export const copilotAPIKey = process.env.COPILOT_API_KEY || ''
-export const apiUrl =
-  process.env.VERCEL_ENV === 'development'
-    ? process.env.API_URL
-    : `https://${process.env.VERCEL_URL}` || ''
+export const apiUrl = `${
+  process.env.VERCEL_ENV === 'development' ? 'http://' : 'https://'
+}${process.env.VERCEL_URL}`
