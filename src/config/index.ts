5b7edc906bcd62581b7d6378d609980db8a54f66
aatbip <anantabipal2010@gmail.com>
2024-01-11 13:06:44 +0545

    refac vercel env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,6 @@
 export const copilotAPIUrl = process.env.COPILOT_API_URL || ''
 export const copilotAPIKey = process.env.COPILOT_API_KEY || ''
-export const apiUrl = process.env.VERCEL_URL || ''
+export const apiUrl =
+  process.env.VERCEL_ENV === 'development'
+    ? process.env.API_URL
+    : `https://${process.env.VERCEL_URL}` || ''
