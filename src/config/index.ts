1a7a594a22651cf907a442d767cb94a60d8d85d9
Rojan Rajbhandari <rrojan20@gmail.com>
2024-02-26 22:38:47 +0545

    chore: add typesafe config object

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,3 +3,7 @@ export const copilotAPIKey = process.env.COPILOT_API_KEY || ''
 export const apiUrl = `${
   process.env.VERCEL_ENV === 'development' ? 'http://' : 'https://'
 }${process.env.VERCEL_URL}`
+export const SentryConfig = {
+  DSN: process.env.SENTRY_DSN,
+  AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
+}
