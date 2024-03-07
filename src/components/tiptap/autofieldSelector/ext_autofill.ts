74cd6c79b6ea5cd6e03fdb6579d55bd880f7a061
aatbip <anantabipal2010@gmail.com>
2024-03-07 22:16:24 +0545

    adds fixes to autofill fields

--- src/components/tiptap/autofieldSelector/ext_autofill.ts
+++ src/components/tiptap/autofieldSelector/ext_autofill.ts
@@ -5,23 +5,10 @@ import { AutofillComponent } from './AutofillComponent'
 export const AutofillExtension = Node.create({
   name: 'autofillComponent',
   group: 'inline',
-  content: 'text*',
+  content: 'inline*',
   inline: true,
   selectable: false,
   atom: true,
-
-  addOptions() {
-    return {
-      HTMLAttributes: {},
-      renderText({ options, node }: any) {
-        return `${node.attrs.label ?? node.attrs.id}`
-      },
-      renderHTML({ options, node }: any) {
-        return ['span', this.HTMLAttributes]
-      },
-    }
-  },
-
   parseHTML() {
     return [
       {
@@ -32,89 +19,11 @@ export const AutofillExtension = Node.create({
 
   whitespace: 'normal',
 
+  renderHTML({ HTMLAttributes }) {
+    return ['autofill', mergeAttributes(HTMLAttributes), 0]
+  },
+
   addNodeView() {
     return ReactNodeViewRenderer(AutofillComponent)
-  },
-
-  addAttributes() {
-    return {
-      id: {
-        default: null,
-        parseHTML: (element) => element.getAttribute('data-id'),
-        renderHTML: (attributes) => {
-          if (!attributes.id) {
-            return {}
-          }
-
-          return {
-            'data-id': attributes.id,
-          }
-        },
-      },
-
-      label: {
-        default: null,
-        parseHTML: (element) => element.getAttribute('data-label'),
-        renderHTML: (attributes) => {
-          if (!attributes.label) {
-            return {}
-          }
-
-          return {
-            'data-label': attributes.label,
-          }
-        },
-      },
-    }
-  },
-
-  renderHTML({ node, HTMLAttributes }) {
-    if (this.options.renderLabel !== undefined) {
-      return [
-        'span',
-        mergeAttributes(
-          { 'data-type': this.name },
-          this.options.HTMLAttributes,
-          HTMLAttributes,
-        ),
-        this.options.renderLabel({
-          options: this.options,
-          node,
-        }),
-      ]
-    }
-    const html = this.options.renderHTML({
-      options: this.options,
-      node,
-    })
-
-    if (typeof html === 'string') {
-      return [
-        'span',
-        mergeAttributes(
-          { 'data-type': this.name },
-          this.options.HTMLAttributes,
-          HTMLAttributes,
-        ),
-        html,
-      ]
-    }
-    return html
-  },
-
-  renderText({ node }) {
-    if (this.options.renderLabel !== undefined) {
-      console.warn(
-        'renderLabel is deprecated use renderText and renderHTML instead',
-      )
-      return this.options.renderLabel({
-        options: this.options,
-        node,
-      })
-    }
-    return this.options.renderText({
-      options: this.options,
-      node,
-    })
   },
 })
